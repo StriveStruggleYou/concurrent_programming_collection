05683aff1051e1d67b51a129de121724d3d9732e
StriveStruggleYou <StriveStruggleYou@163.com>
2018-06-17 06:53:42 +0800

    代码细节学习

--- disruptor_demo/src/main/java/org/ssy/demo5/LongEventProducer.java
+++ disruptor_demo/src/main/java/org/ssy/demo5/LongEventProducer.java
@@ -6,7 +6,6 @@ import java.nio.ByteBuffer;
 /**
  * Created by manager on 2018/6/16.
  * 事件的生产者
- *
  */
 public class LongEventProducer {
 
@@ -17,13 +16,29 @@ public class LongEventProducer {
   }
 
   public void onData(ByteBuffer bb) {
-    long sequence = ringBuffer.next();  // Grab the next sequence
+
+
+    System.out.println("ringBuffer:" + ringBuffer.getMinimumGatingSequence() + "position:"+ringBuffer.remainingCapacity());
+
+    long startTime = System.currentTimeMillis();
+    long sequence = ringBuffer.next();
+
+    long getSequence = System.currentTimeMillis();
+    System.out.println("getSequence:" + (getSequence - startTime) + "ms");
+
+    // Grab the next sequence
     try {
       LongEvent event = ringBuffer.get(sequence); // Get the entry in the Disruptor
+
+      long getEvent = System.currentTimeMillis();
+      System.out.println("getEvent:" + (getEvent - getSequence) + "ms");
       // for the sequence
       event.set(bb.getLong(0));  // Fill with data
     } finally {
       ringBuffer.publish(sequence);
+
+      System.out.println("LongEventProducer:" + (System.currentTimeMillis() - getSequence) + "ms");
+
     }
   }
 }
