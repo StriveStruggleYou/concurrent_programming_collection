d574d7169093b1f162bd121e55046d3b9fcf2788
StriveStruggleYou <StriveStruggleYou@163.com>
2018-06-16 18:01:47 +0800

    生产事件

--- disruptor_demo/src/main/java/org/ssy/demo5/LongEventFactory.java
+++ disruptor_demo/src/main/java/org/ssy/demo5/LongEventFactory.java
@@ -1,8 +1,15 @@
 package org.ssy.demo5;
 
+import com.lmax.disruptor.EventFactory;
+
 /**
  * Created by manager on 2018/6/16.
+ * 定义一个事件工厂，创建事件
+ *
  */
-public class LongEventFactory {
+public class LongEventFactory implements EventFactory<LongEvent> {
 
+  public LongEvent newInstance() {
+    return new LongEvent();
+  }
 }
