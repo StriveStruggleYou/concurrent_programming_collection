bd992f1df11cc2dfc0ce9bf7c9d88c6579ba4c40
StriveStruggleYou <StriveStruggleYou@163.com>
2018-06-16 17:59:58 +0800

    新增基础数据

--- disruptor_demo/src/main/java/org/ssy/demo5/LongEvent.java
+++ disruptor_demo/src/main/java/org/ssy/demo5/LongEvent.java
@@ -2,7 +2,22 @@ package org.ssy.demo5;
 
 /**
  * Created by manager on 2018/6/16.
+ *
+ * firstly we will define the Event that will carry the data.
+ * 首先定义一个事件以及承载的数据
  */
 public class LongEvent {
 
+  private long value;
+
+  public void set(long value) {
+    this.value = value;
+  }
+
+  @Override
+  public String toString() {
+    return "LongEvent{" +
+        "value=" + value +
+        '}';
+  }
 }
