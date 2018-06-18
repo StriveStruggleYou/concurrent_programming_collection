b87a239599391b7a9cf124acd3a1860ca6e06d41
StriveStruggleYou <StriveStruggleYou@163.com>
2018-06-18 23:28:16 +0800

    新增注释信息

--- disruptor_demo/src/main/java/org/ssy/demo7/DslDemo.java
+++ disruptor_demo/src/main/java/org/ssy/demo7/DslDemo.java
@@ -1,8 +1,49 @@
 package org.ssy.demo7;
 
+import com.lmax.disruptor.RingBuffer;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
 /**
  * Created by manager on 2018/6/18.
  */
 public class DslDemo {
+
+  public static void main(){
+//    Executor executor = Executors.newCachedThreadPool();
+//
+//    BatchHandler handler1 = new MyBatchHandler1();
+//
+//    BatchHandler handler2 = new MyBatchHandler2();
+//
+//    BatchHandler handler3 = new MyBatchHandler3()
+//
+//    RingBuffer ringBuffer = new RingBuffer(ENTRY_FACTORY, RING_BUFFER_SIZE);
+//
+//    ConsumerBarrier consumerBarrier1 = ringBuffer.createConsumerBarrier();
+//
+//    BatchConsumer consumer1 = new BatchConsumer(consumerBarrier1, handler1);
+//
+//    BatchConsumer consumer2 = new BatchConsumer(consumerBarrier1, handler2);
+//
+//    ConsumerBarrier consumerBarrier2 =
+//
+//    ringBuffer.createConsumerBarrier(consumer1, consumer2);
+//
+//    BatchConsumer consumer3 = new BatchConsumer(consumerBarrier2, handler3);
+//
+//    executor.execute(consumer1);
+//
+//    executor.execute(consumer2);
+//
+//    executor.execute(consumer3);
+//
+//    ProducerBarrier producerBarrier =
+//    ringBuffer.createProducerBarrier(consumer3);
+
+
+
+  }
+
 
 }
