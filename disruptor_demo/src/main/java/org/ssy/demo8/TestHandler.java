6bc6946206bf41115022a7998a5cce3e581daf89
StriveStruggleYou <StriveStruggleYou@163.com>
2018-06-18 23:35:45 +0800

    提交测试基础代码

---
+++ disruptor_demo/src/main/java/org/ssy/demo8/TestHandler.java
@@ -0,0 +1,23 @@
+package org.ssy.demo8;
+
+/**
+ * Created by manager on 2018/6/18.
+ */
+public class TestHandler {
+
+  private CounterTracer tracer;
+
+  public TestHandler(CounterTracer tracer) {
+    this.tracer = tracer;
+  }
+
+  /**
+   * 如果返回 true，则表示处理已经全部完成，不再处理后续事件；
+   *
+   * @param event
+   * @return
+   */
+  public boolean process(TestEvent event){
+    return tracer.count();
+  }
+}
